1bcba3d868047fc2de20d8a6b58bf42edcbbcb64
Jocelyn Lee <biheyeblue@gmail.com>
2021-01-03 19:42:19 +0800

    fixed the sign_in problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, 
+         :recoverable, :rememberable, :validatable, 
          :confirmable
 
          
-         #,:omniauthable
+         #,:omniauthable :trackable, 
 end
