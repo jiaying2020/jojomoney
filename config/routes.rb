eb6ef471c5293599c247873fe06add797b4a6c0a
Jocelyn Lee <biheyeblue@gmail.com>
2021-01-03 17:16:55 +0800

    add devise controller users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    # omniauth_callbacks: "users/omniauth_callbacks",
+    unlocks: 'users/unlocks'
+  }
   root to: "home#index"
 
 end
