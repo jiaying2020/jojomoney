92ccecfef2540d7f2eed2c46e70b7401f2c4bf95
Jocelyn Lee <biheyeblue@gmail.com>
2021-01-03 12:55:43 +0800

    add  bootstrap and jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,9 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import $ from 'jquery';
+window.$ = $;
