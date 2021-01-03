1641153bd936d75c42ffec908a44468efca87e3b
Jocelyn Lee <biheyeblue@gmail.com>
2021-01-03 17:34:39 +0800

    add migration confirmable_to_devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
     registrations: 'users/registrations',
     confirmations: 'users/confirmations',
     passwords: 'users/passwords',
     # omniauth_callbacks: "users/omniauth_callbacks",
     unlocks: 'users/unlocks'
   }
+  
   root to: "home#index"
 
 end
