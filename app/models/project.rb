c2f083d67a02086bf03024b9803245d81d6f529c
Jocelyn Lee <biheyeblue@gmail.com>
2021-01-06 23:38:03 +0800

    fixed enum and scope in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,9 +9,11 @@ class Project < ApplicationRecord
     
     enum status: [:is_hidden, :is_published ,:succeeded, :failed, :cancel]
     
-    scope :is_now_on_sale, -> {self.is_published.where(status:[:is_published,:succeeded]).where('due_date > ?', Time.now)}
-    scope :succeeded_and_done, ->{self.succeeded.where('due_date < ?', Time.now)}
-    scope :past_projects, -> {self.where.not(status: [:cancel,:is_hidden]).where('due_date > ?', Time.now)}
+    scope :is_now_on_sale, -> {self.where(status:[:is_published,:succeeded]).where('due_date > ?', Time.now)}
+    scope :succeeded_and_done, -> {self.succeeded.where('due_date < ?', Time.now)}
+    scope :past_projects, -> {self.where.not(status: [:cancel,:is_hidden]).where('due_date < ?', Time.now)}
+
+    #取消跟未上架不會出現，not指不包括:cancel,:is_hidden
 
     
     mount_uploader :cover_image, CoverImageUploader
@@ -38,7 +40,7 @@ class Project < ApplicationRecord
     private
     
     def valid_due_date?
-      if due_date.blank? || due_date < ((created_at || Time.zone.now))
+      if due_date.blank? || due_date < ((created_at || Time.zone.now)+ 7.days)
         errors.add(:due_date, "due_date is not correct")
       end
     end
